567fc610fa799ea256c2bfbcbc70f27e2cd47ffc
Salomón Zegarra <salozm34@gmail.com>
2023-06-12 09:15:21 -0500

    Initial commit

---
+++ landing-page/src/constants/index.js
@@ -0,0 +1,50 @@
+export const navLinks = [
+    {
+        id:"about",
+        title: "sobre nosotros",
+    },
+    {
+        id:"bussines",
+        title: "unidades de negocio",
+    },
+    {
+        id:"sector",
+        title: "sectores",
+    },
+    {
+        id:"security_courses",
+        title: "cursos de seguridad",
+    },
+    {
+        id:"clients",
+        title: "clientes",
+    },
+    {
+        id:"contact",
+        title: "contactanos",
+    }
+];
+
+export const footerLinks = [
+    {
+        title: "ACCESOS DIRECTOS",
+        links: [
+            {
+                name: "Trabaja con nosotros",
+                link: "https://www.essac.com.pe/trabaja-con-nosotros/",
+            },
+            {
+                name: "Novedades",
+                link: "https://www.essac.com.pe/novedades/",
+            },
+            {
+                name: "Catálogo digital de cursos",
+                link: "https://www.essac.com.pe/catalago/",
+            },
+            {
+                name: "Brochure comercial",
+                link: "https://www.essac.com.pe/brochure/",
+            },
+        ],
+    },
+]
